0130d0555512a62779f07984c6531e6a3bb77923
rrmettu <rmettu@tulane.edu>
2020-08-21 17:01:50 +0000

    added test

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def sum_of_squares(a):
-	pass
+	return sum([x*x for x in a])
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+
+def test_two():
+    assert sum_of_squares([0]) == 0
+
+def test_three():
+    assert sum_of_squares([1, 2, 3, 4]) == 30
+    
